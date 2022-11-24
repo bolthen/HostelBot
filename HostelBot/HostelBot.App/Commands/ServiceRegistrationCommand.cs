84ebe3b2eaf66529cd80f909c6bcbadbaf2e5474
natosafix <92174064+natosafix@users.noreply.github.com>
2022-11-24 16:38:41 +0500

    Add entity to infrastucture

---
+++ HostelBot/HostelBot.App/Commands/ServiceRegistrationCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace HostelBot.App;
+
+public class ServiceRegistrationCommand : ICommand
+{
+
+
+    public void HandleCommand(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
