b520c6f0425a8b15f542978fdd0c3dfaf1df2b7f
natosafix <92174064+natosafix@users.noreply.github.com>
2022-12-14 22:34:52 +0500

    Add ResidentManager

--- HostelBot/HostelBot.Domain/Infrastructure/Managers/ResidentManager.cs
+++ HostelBot/HostelBot.Domain/Infrastructure/Managers/ResidentManager.cs
@@ -1,11 +1,19 @@
 ﻿using HostelBot.Domain.Domain;
+using HostelBot.Domain.Infrastructure.Services;
 
 namespace HostelBot.Domain.Infrastructure.Managers;
 
 public class ResidentManager : Manager<Resident>
 {
+    private readonly ResidentService residentService;
+
+    public ResidentManager(ResidentService residentService)
+    {
+        this.residentService = residentService;
+    }
+    
     protected override void Handle(Resident value)
     {
-        throw new NotImplementedException();
+        residentService.CreateAsync(value);
     }
 }
