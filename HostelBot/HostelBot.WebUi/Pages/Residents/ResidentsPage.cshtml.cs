d6a7de76ec5e4719445323ab335a38bf5bb9fb86
bolthen <92172378+bolthen@users.noreply.github.com>
2022-12-18 02:38:47 +0500

    Merge with not workable DbRework

--- HostelBot/HostelBot.WebUi/Pages/Residents/ResidentsPage.cshtml.cs
+++ HostelBot/HostelBot.WebUi/Pages/Residents/ResidentsPage.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using HostelBot.Domain.Domain;
 using HostelBot.Domain.Infrastructure.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -10,39 +11,38 @@ namespace WebUi.Pages.Residents
     {
         public IReadOnlyCollection<Resident> Residents { get; set; }
 
-        private readonly ResidentRepository residentRepository;
+        private readonly Hostel? hostel;
 
-        public ResidentsPage(ResidentRepository residentRepository)
+        public ResidentsPage(HostelRepository hostelRepository)
         {
-            //var hostel = hostelRepository.GetAsync("№6").Result.Residents;
-            this.residentRepository = residentRepository;
-        
-            var hostel = new Hostel("№6");
-            var a = new Room(718, hostel);
-            var b = new Room(719, hostel);
-        
-        
-            var mockBd = new List<Resident>
-            {
-                new (1, "John", "Johnson", hostel, new Room(718, hostel)),
-                new (1, "John", "Johnson", hostel, new Room(718, hostel)),
-                new (5, "Sam", "Samson", hostel, a),
-                new (3, "Shon", "Shonson", hostel, b),
-                new (4, "Wayn", "Waynson", hostel, b)
-                
-            };
+            var tmpId = 1; // TODO User.Claim
+            hostel = hostelRepository.GetAsync(tmpId).Result;
 
-            foreach (var resident in mockBd)
-            {
-                this.residentRepository.CreateAsync(resident);
-            }
+            //var hostel = hostelRepository.GetAsync("№6").Result.Residents;
+            // this.residentRepository = residentRepository;
+            //
+            // var hostel = new Hostel("№6");
+            // var a = new Room(718, hostel);
+            // var b = new Room(719, hostel);
+            //
+            //
+            // var mockBd = new List<Resident>
+            // {
+            //     new (1, "John", "Johnson", hostel, new Room(718, hostel)),
+            //     new (1, "John", "Johnson", hostel, new Room(718, hostel)),
+            //     new (5, "Sam", "Samson", hostel, a),
+            //     new (3, "Shon", "Shonson", hostel, b),
+            //     new (4, "Wayn", "Waynson", hostel, b)
+            //     
+            // };
+            //
+            // foreach (var resident in mockBd)
+            //     residentRepository.CreateAsync(resident);
         }
     
         public void OnGet()
         {
-            Residents = residentRepository.GetAll().Result.ToArray();
-            // Подгружаем жителей из БД и парсим в табличку,
-            // к которой будем обращаться в отображении
+            Residents = hostel?.Residents.ToArray() ?? Array.Empty<Resident>();
         }
     }
 }
